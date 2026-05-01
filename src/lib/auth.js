c1cb3663245337c791f491bb96fcf419a9ca701b
miraj <mirajhowlader9999@gmail.com>
2026-05-01 21:23:46 +0600

    fix auth config

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,21 +2,26 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGODB_URI);
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI is not defined");
+}
+
+const client = new MongoClient(uri);
 const db = client.db();
 
 export const auth = betterAuth({
-    emailAndPassword:{
-        enabled:true
+  emailAndPassword: {
+    enabled: true,
+  },
+  socialProviders: {
+    google: {
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_SECRET_ID,
     },
-    socialProviders:{
-         google: { 
-            clientId: process.env.GOOGLE_CLIENT_ID, 
-            clientSecret: process.env.GOOGLE_SECRET_ID , 
-        }, 
-    },
+  },
   database: mongodbAdapter(db, {
-    // Optional: if you don't provide a client, database transactions won't be enabled.
-    client
+    client,
   }),
 });
